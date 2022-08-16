58f69033213d91c42df40d07df5de79c52b00091
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2022-08-16 20:51:16 +0300

    class work 13
    курс валют

--- class_works/cw 13/js/code.js
+++ class_works/cw 13/js/code.js
@@ -1,30 +1,31 @@
 "use script";
-//const url = `https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json`;
-//const request = new XMLHttpRequest();
+const url = `https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json`;
+const request = new XMLHttpRequest();
 
-//request.open(`GET`, url);
-//request.send();
+request.open(`GET`, url);
+request.send();
 
-//request.addEventListener(`readystatechange`, () => {
-//    if (request.readyState === 4 && request.status >= 200 && request.status < 300) {
-//        makeTable(JSON.parse(request.responseText))
-//    }
-//})
+request.addEventListener(`readystatechange`, () => {
+    if (request.readyState === 4 && request.status >= 200 && request.status < 300) {
+        makeTable(JSON.parse(request.responseText))
+    }
+})
 
-//function makeTable(arr){
-//    arr.forEach((item) => {
-//        const {txt, rate} = item
-//        let tr = document.createElement(`tr`),
-//            tdTxt = document.createElement(`td`),
-//            tdRate = document.createElement(`td`);
+function makeTable(arr){
+    arr.forEach((item) => {
+        console.log(item);
+        const {txt, rate} = item
+        let tr = document.createElement(`tr`),
+            tdTxt = document.createElement(`td`),
+            tdRate = document.createElement(`td`);
 
-//        tdTxt.innerText = txt;
-//        tdRate.innerText = rate;
-//        tr.append(tdTxt, tdRate);
-//        document.querySelector(`tbody`).append(tr);
+        tdTxt.innerText = txt;
+        tdRate.innerText = rate;
+        tr.append(tdTxt, tdRate);
+        document.querySelector(`tbody`).append(tr);
 
-//    })
-//}
+    })
+}
 //=======================================================
 
 
@@ -40,15 +41,15 @@
 
 
 
-const url = `https://swapi.dev/api/planets/`
+//const url = `https://swapi.dev/api/planets/`
 
-//создаем запрос на сервер с помощбю fetch
-const data = fetch(url, {method: `get`});
+////создаем запрос на сервер с помощбю fetch
+//const data = fetch(url, {method: `get`});
 
-//работа с запросом. обробатываем как promice
-const data1 = data.then((promice) => promice.json(), (error) => console.error(error))
+////работа с запросом. обробатываем как promice
+//const data1 = data.then((promice) => promice.json(), (error) => console.error(error))
 
-data1.then((x) => {
-    document.write(x.results[0].name)
-    console.log(x);
-})
+//data1.then((x) => {
+//    document.write(x.results[0].name)
+//    console.log(x);
+//})
